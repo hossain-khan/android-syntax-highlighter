8a5382d7c86aea8c76d611e8af58dfcb6194c3ae
Hossain Khan <amardeshbd@gmail.com>
2020-07-16 18:36:15 -0400

    [ADDED] Prism JS based syntax highlighter
    With dark mode support.
    
    Most of the code is taken from earlier experiment I did in following project
    * https://github.com/amardeshbd/android-recyclerview-onclick-listener/pull/11
    * https://github.com/amardeshbd/android-recyclerview-onclick-listener/pull/14

---
+++ highlighter/src/main/java/dev/hossain/yaash/webclient/ShowSourceCodeFragment.kt
@@ -0,0 +1,43 @@
+package dev.hossain.yaash.webclient
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.fragment.app.Fragment
+import androidx.webkit.WebSettingsCompat
+import androidx.webkit.WebViewFeature
+import dev.hossain.yaash.R
+
+class ShowSourceCodeFragment : Fragment() {
+    companion object {
+        private const val ANDROID_ASSETS_PATH = "file:///android_asset/"
+    }
+
+    private lateinit var webView: WebView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_highlighter, container, false)
+        webView = rootView.findViewById(R.id.web_view)
+        return rootView
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // https://stackoverflow.com/questions/57449900/letting-webview-on-android-work-with-prefers-color-scheme-dark?rq=1
+        if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+            WebSettingsCompat.setForceDark(webView.settings, WebSettingsCompat.FORCE_DARK_ON)
+        }
+
+        webView.apply {
+            settings.javaScriptEnabled = true
+            webChromeClient = WebViewChromeClient()
+            webViewClient = AppWebViewClient()
+            loadUrl("${ANDROID_ASSETS_PATH}_template.html")
+        }
+    }
+}
